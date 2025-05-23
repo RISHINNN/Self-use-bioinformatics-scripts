8232980a9c437520b6e80eb7f0e3f627f8a3ccc1
Biols <50230939+Biols0208@users.noreply.github.com>
2025-05-23 15:38:33 +0800

    Update syri_plotsv.sh

--- picture/syri_plotsv/syri_plotsv.sh
+++ picture/syri_plotsv/syri_plotsv.sh
@@ -1,13 +1,20 @@
 ## https://github.com/nschan/nf-plotsv 
 
-ref_genome=Chr.male.fasta
-ref_genome_chr_num=19
-query_genome=manli_telo.chr_genome.fa
-query_genome_chr_num=19
-cpu_for_minimap=4
-mem_for_minimap=20
+## Use relative paths
+ref_genome=final-father-chr-mela.fa
+ref_genome_chr_num=24
+query_genome=final-mother-chr-mela.fa
+query_genome_chr_num=24
+cpu_for_minimap=20
+mem_for_minimap=100
+plot_all_chr_num=24
+plotsr_Space_for_homologous_chromosome=0.7
+plotsr_height=10
+plotsr_width=7
+plotsr_font_size=8
+plotsr_minimum_size_of_SR_to_be_plotted=5000
 
-export PATH="/dellfsqd2/ST_OCEAN/USER/lishuo1/00_tools/:$PATH"
+export PATH="/00_tools/:$PATH"
 
 ## rename chromesome
 iTools Fatools stat -InPut ${ref_genome} -OutPut ${ref_genome}.chrlen
@@ -17,13 +24,29 @@ grep -v "#" ${query_genome}.chrlen | sort -k 2nr,2 | head -n ${query_genome_chr_
 
 ## make samplesheet
 echo "name,fasta" >> samplesheet.csv
-echo "genome1,$PWD/ref.rename.fa" >> samplesheet.csv
-echo "genome2,$PWD/query.rename.fa" >> samplesheet.csv
+## the names also be used in the final picture.
+echo "ref,$PWD/ref.rename.fa" >> samplesheet.csv
+echo "query,$PWD/query.rename.fa" >> samplesheet.csv
 
 ## run
-cp base.demo.config base.config
+cp /01_soft/nf-plotsv/configs/base.demo.config base.config
 sed -i "s/ABCD/${cpu_for_minimap}/g;s/EFGH/${mem_for_minimap}/g" base.config
-nextflow -config $PWD/base.config run /dellfsqd2/ST_OCEAN/USER/lishuo11/01_soft/nf-plotsv --samplesheet samplesheet.csv -profile local --reference ref_genome --ref_genome $PWD/ref.rename.fa --subset_pattern Chr[1-9] --reorient true
+nextflow -config $PWD/base.config run /01_soft/nf-plotsv --samplesheet samplesheet.csv -profile local --reference ref_genome --ref_genome $PWD/ref.rename.fa --subset_pattern Chr[1-9] --reorient true
+
+## re-draw
+cp work/*/*/plotsr_infile.tsv plotsv/syri_pairwise/plotsr_infile.tsv
+cp work/*/*/files.txt plotsv/syri_pairwise/files.txt
+cp /nf-plotsv/assets/plotsr_config.conf plotsv/syri_pairwise/plotsr_config.conf
+for i in `seq 1 ${plot_all_chr_num}` ; do echo Chr${i} >> plotsv/syri_pairwise/chr.order ; done
+
+cd plotsv/syri_pairwise/
+cp ../align_pairwise/*fa ./
+sr=$(cat files.txt)
+singularity run /01_soft/singularity_all/fixchr-syri-plotsr.sif plotsr --genomes plotsr_infile.tsv ${sr} --cfg plotsr_config.conf -o replot.pdf -S ${plotsr_Space_for_homologous_chromosome} -W ${plotsr_width} -H ${plotsr_height} -f ${plotsr_font_size} -s ${plotsr_minimum_size_of_SR_to_be_plotted} --chrord chr.order
+pigz --best -p ${cpu_for_minimap} *syri.out
+pigz --best -p ${cpu_for_minimap} *syri.vcf
+pigz --best -p ${cpu_for_minimap} *.fa
+cd ../../
 
 ## rm tmp
-rm -rf .nextflow* work ref.rename.fa query.rename.fa *chrlen *.chrlist  samplesheet.csv base.config
+rm -rf .nextflow* work ref.rename.fa query.rename.fa *chrlen *.chrlist  samplesheet.csv base.config plotsv/prepare_genomes plotsv/align_pairwise
